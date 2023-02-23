120448cbc6636e3dc01869d2173dbe120d46b970
wbach <91107968+Will-Bach90@users.noreply.github.com>
2023-02-23 02:00:38 -0800

    Update book.h

--- item/book.h
+++ item/book.h
@@ -1,3 +1,14 @@
+// Parent class for all books contained in the library
+//---------------------------------------------------------------------------
+// Book class:  
+//   -- (Virtual) Getters/Setters for all data members and pure virtual
+//      getters/setters for use in derived classes
+//
+// Implementation and assumptions:
+//   -- Pure virtual functions to be defined in derived classes
+//   -- Book class not intended to be called directly other than as a pointer
+//      to a derived class.
+//---------------------------------------------------------------------------
 #ifndef BOOK
 #define BOOK
 
@@ -18,13 +29,14 @@ class Book : public Item
         // Getters/Setters
         virtual int getYear() const;
         virtual char getFormat() const;
+        virtual char getType() const;
 
         virtual void setYear(int);
         virtual void setFormat(char);
 
-        virtual string getLastName() const = 0;
-        virtual string getFirstName() const = 0;
-        virtual int getMonth() const = 0;
+        virtual string getLastName() const = 0;     //Pure virtual
+        virtual string getFirstName() const = 0;    //Pure virtual
+        virtual int getMonth() const = 0;           //Pure virtual
 
         //---------------------------------------------------------------------
         // For use with bookfactory class (pure virtual)
@@ -34,6 +46,8 @@ class Book : public Item
         // sets book data from data file (pure virtual)
         virtual void setData(string) = 0;
 
+        //---------------------------------------------------------------------
+        // sets item data from transaction data file (pure virtual)
          virtual void setTransactionData(string) = 0;
 
         //---------------------------------------------------------------------
@@ -48,8 +62,9 @@ class Book : public Item
         virtual bool operator>(const Item &) const = 0;
 
     protected:
+        // further data members for book items
         int year;
-        char bookFormat;
+        char bookType;
 };
 
 #endif
