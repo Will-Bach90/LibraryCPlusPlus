bf4b89834c168d59ffcc2b2625b66fe5dbc62724
wbach <91107968+Will-Bach90@users.noreply.github.com>
2023-02-23 03:02:40 -0800

    Update list.h

--- list/list.h
+++ list/list.h
@@ -1,3 +1,12 @@
+//---------------------------------------------------------------------------
+// list.h
+// Parent list class for implementing linked lists
+//---------------------------------------------------------------------------
+// List class Implementation and assumptions:  
+//   -- List is a pure virtual class, not intended to be called directly.
+//   -- Implementation is left to derived classes.
+//
+//---------------------------------------------------------------------------
 #ifndef LIST
 #define LIST
 
@@ -24,6 +33,8 @@ class List
         // Pure virtual function to print out entire list
         virtual void print() const = 0;
 
+        //---------------------------------------------------------------------
+        // Pure virtual function for deleting list
         virtual void makeEmpty() = 0;
 
 };
