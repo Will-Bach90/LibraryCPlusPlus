1ec476a63f109284797d2f8d5648fcb37d5c6780
wbach <91107968+Will-Bach90@users.noreply.github.com>
2023-02-23 03:08:22 -0800

    Update patronlist.h

--- list/patronlist.h
+++ list/patronlist.h
@@ -1,3 +1,13 @@
+//---------------------------------------------------------------------------
+// patronlist.h
+// Derived list class for saving patrons in a linked list
+//---------------------------------------------------------------------------
+// PatronList class Implementation and assumptions:  
+//   -- PatronClass used for collision handling in PatronHashMap class
+//   -- Currently only allows append and retrieve functions. Patron's not
+//      assumed to be removed from library database.
+//
+//---------------------------------------------------------------------------
 #ifndef PATRON_LIST
 #define PATRON_LIST
 
@@ -19,10 +29,15 @@ class PatronList : public List
         // Virtual function to add patron to end of list
         virtual void append(Patron*);
 
+        //---------------------------------------------------------------------
+        // Function to retrieve patron given their id. Set second parameter as 
+        // pointer to patron if found. Returns true if found
         virtual bool retrieve(int, Patron*&);
 
+        //---------------------------------------------------------------------
+        // deletes entire list
         void makeEmpty();
-
+        
         //---------------------------------------------------------------------
         // Function to print out entire list of patrons
         virtual void print() const;
