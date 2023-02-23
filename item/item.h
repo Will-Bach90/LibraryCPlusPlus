9d0cb11deaaf64226d937a5e0f8e151221acaac7
wbach <91107968+Will-Bach90@users.noreply.github.com>
2023-02-23 01:17:32 -0800

    Update item.h

--- item/item.h
+++ item/item.h
@@ -1,3 +1,19 @@
+//---------------------------------------------------------------------------
+// item.h
+// Parent class for all items for use/checkout available in 
+// the library
+//---------------------------------------------------------------------------
+// Item class:  
+//   includes additional features:
+//   -- (Virtual) Getters/Setters for all data members
+//   -- Overloaded output operator for use with virtual 
+//   -- allows for assignment of 2 arrays
+//   -- size is part of the class (so no longer needs to be passed)
+//   -- includes range checking, program terminates for out-of-bound subscripts
+//
+// Implementation and assumptions:
+//   -- Pure virtual functions to be defined in derived classes
+//---------------------------------------------------------------------------
 #ifndef ITEM
 #define ITEM
 
@@ -16,20 +32,21 @@ class Item
         Item();
 
         //---------------------------------------------------------------------
-        // Destructor
+        // Virtual destructor: items contained in the library are assigned as
+        // Item* pointing to the derived class, so Item's destructor must
+        // be virtual.
         virtual ~Item();
-
-        
-         //---------------------------------------------------------------------
-         // Getters/Setters
+    
+        //---------------------------------------------------------------------
+        // Getters/Setters
         virtual string getTitle() const;
         virtual bool inStock() const;
         virtual string getType() const;
         virtual int getNumCopies() const;
-        virtual int getYear() const = 0;
-        virtual string getLastName() const = 0;
-        virtual string getFirstName() const = 0;
-        virtual int getMonth() const = 0;
+        virtual int getYear() const;
+        virtual string getLastName() const;
+        virtual string getFirstName() const;
+        virtual int getMonth() const;
 
         virtual void setTitle(string);
         virtual void setNumCopies(int);
@@ -40,9 +57,11 @@ class Item
         virtual Item* create() const = 0; 
     
         //---------------------------------------------------------------------
-        // sets item data from data file (pure virtual)
+        // sets item data from book data file (pure virtual)
         virtual void setData(string) = 0;
-
+    
+        //---------------------------------------------------------------------
+        // sets item data from transaction data file (pure virtual)
         virtual void setTransactionData(string) = 0;
 
         //---------------------------------------------------------------------
@@ -57,8 +76,13 @@ class Item
         virtual bool operator>(const Item &) const = 0;
 
     protected:
+        // Data members for use in derived classes
+        string firstName; 
+        string lastName;  
+        int year;
+        int month;
         string title;
-        string itemType;
+        char itemType;
         int copies;
 };
 
