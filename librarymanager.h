3dc78ad30dee972432b5916eb278f37c188c7d19
wbach <91107968+Will-Bach90@users.noreply.github.com>
2023-02-23 03:38:22 -0800

    Update librarymanager.h

--- librarymanager.h
+++ librarymanager.h
@@ -1,3 +1,17 @@
+//---------------------------------------------------------------------------
+// librarymanager.h
+// Primary manager class to be called by the main. Handles building of the
+// library, creating list of patrons, and processing transactions.
+//---------------------------------------------------------------------------
+// LibraryManager class Implementation and assumptions:  
+//   -- Data members include the hashmap of patrons, hash table of binary 
+//      trees holding all books, book and transaction factory objects for
+//      instantiating book and transaction objects.
+//   -- Hash function maps char of book type (C for childrens book, F for
+//      fiction, and P for Periodical) to indexes in the array of Binary 
+//      Trees. Assumes 1:1 mapping (closed hashing), no collision handling
+//
+//---------------------------------------------------------------------------
 #ifndef LIBRARY_MANAGER_H
 #define LIBRARY_MANAGER_H
 
@@ -11,10 +25,10 @@ class LibraryManager
     public:
         LibraryManager();
         ~LibraryManager();
-        void processTransactions(string fileName);
-        void addBooks(string fileName);
-        void addPatrons(string fileName);
-        void print(char) const;
+        void processTransactions(istream&);
+        void addBooks(istream&);
+        void addPatrons(istream&);
+        void print(char) const; // prints out entire library
 
     private:
         PatronList patrons;
