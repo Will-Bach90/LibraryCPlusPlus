28cdc9915ea83861f499e7a7ed38d54eaebcd72e
wbach <91107968+Will-Bach90@users.noreply.github.com>
2023-02-23 03:22:03 -0800

    Update transaction.h

--- transactions/transaction.h
+++ transactions/transaction.h
@@ -1,3 +1,19 @@
+//---------------------------------------------------------------------------
+// transaction.h
+// Parent transaction class for main transaction types: checkout, return, 
+// and display history
+//---------------------------------------------------------------------------
+// Transaction class Implementation and assumptions:  
+//   -- Transaction class not intended to be called directly other than as
+//      a pointer to derived classes.
+//   -- Data members include pointer to Item object upon which it is acting, 
+//      pointer to patron who is taking part in transaction, a bool type for 
+//      confirming whether book data member is in library, and char for 
+//      transaction type ('C' for checkout, 'R' for return, 'H' for 
+//      displaying history)
+//   -- Transaction* create to be implemented in derived classes, returns 
+//      pointer to transaction object.
+//---------------------------------------------------------------------------
 #ifndef TRANSACTION
 #define TRANSACTION
 
@@ -17,7 +33,7 @@ class Transaction
 
         //---------------------------------------------------------------------
         // Destructor
-        ~Transaction();
+        virtual ~Transaction();
 
         //---------------------------------------------------------------------
         // Getters/Setters
