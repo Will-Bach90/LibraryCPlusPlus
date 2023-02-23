3b1175c74d4404216f045cc34351e4c6e4a135d3
wbach <91107968+Will-Bach90@users.noreply.github.com>
2023-02-23 02:59:30 -0800

    Update transactionfactory.h

--- factory/transactionfactory.h
+++ factory/transactionfactory.h
@@ -1,3 +1,16 @@
+// Derived class of Factory class for returning transaction objects
+//---------------------------------------------------------------------------
+// Transactionfactory class Implementation and assumptions:  
+//   -- Item* createObject(char) function returns new Transaction object, 
+//      where the type of transaction is determined by input char 
+//      ('C' for checkout, 'R' for return, 'H' for displaying a patron's
+//      history).
+//   -- objFactory data member is array of Transaction*, where each each 
+//      index maps to a particular transaction type. 
+//   -- Hashing function used to map a given char value to index of array
+//   -- Invalid input chars return null pointers
+//
+//---------------------------------------------------------------------------
 #ifndef TRANSACTION_FACTORY
 #define TRANSACTION_FACTORY
 
