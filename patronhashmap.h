dee107e25ba5b1f420bf7dfd12286d0ad5c29e1d
wbach <91107968+Will-Bach90@users.noreply.github.com>
2023-02-23 03:43:55 -0800

    Update patronhashmap.h

--- patronhashmap.h
+++ patronhashmap.h
@@ -1,3 +1,17 @@
+//---------------------------------------------------------------------------
+// patronhashmap.h
+// Hash table to store all patrons. 
+//---------------------------------------------------------------------------
+// PatronHashMap class Implementation and assumptions:  
+//   -- Hash function computes index of array based on patron's id
+//   -- Open hashing, collisions handled via linked lists stored in each slot
+//      of array rather than the patrons themselves. 
+//   -- Memory allocated dynamically as more patrons are added to the hash 
+//      table.
+//   -- Assumes patron's ids are determined randomly to maintain fast lookup 
+//      ~O(1) time.
+//
+//---------------------------------------------------------------------------
 #ifndef PATRON_HASHMAP
 #define PATRON_HASHMAP
 
@@ -34,7 +48,7 @@ class PatronHashMap
         PatronList* table[TABLE_SIZE]; // the hash table
         //---------------------------------------------------------------------
         // Hash function
-        int hash(int);
+        int hash(int) const;
 };
 
 #endif
