8b59fdc96fc5717d104784a197b752a5622d6dd3
wbach <91107968+Will-Bach90@users.noreply.github.com>
2023-02-23 02:59:44 -0800

    Update bookfactory.h

--- factory/bookfactory.h
+++ factory/bookfactory.h
@@ -6,8 +6,6 @@
 //   -- Item* createObject(char) function returns new Book object, where the
 //      type of book is determined by input char ('C' for ChildrensBook,
 //      'F' for Fiction, 'P' for periodical).
-//   -- setData function sets all data members for fiction book from 
-//      data file
 //   -- objFactory data member is array of Item*, where each each index maps 
 //      to a particular book type. 
 //   -- Hashing function used to map a given char value to index of array
