dee64475b5161538b060655d3196a7649662575a
wbach <91107968+Will-Bach90@users.noreply.github.com>
2023-02-23 00:28:25 -0800

    Update factory.h

--- factory/factory.h
+++ factory/factory.h
@@ -1,17 +1,35 @@
+//---------------------------------------------------------------------------
+// factory.h
+// Parent Factory class for use with derived BookFactory and 
+// TransactionFactory classes
+//---------------------------------------------------------------------------
+// Factory class:  Parent factory class with minimal functionality. 
+//
+// Implementation and assumptions:
+//      -- Implementation is largely left to derived classes
+//      -- Hash function assumes size of hash table in child classes to 
+//         to be 36, with unique (closed) hashing.
+//      -- No collision handling.
+//---------------------------------------------------------------------------
+
 #ifndef FACTORY
 #define FACTORY
 
 #include <iostream>
 using namespace std;
 
-const int CHARACTERS = 26;
+// size of hash tables in derived classes (36 chars: A-Z, 0-9)
+const int CHARACTERS = 36; 
 
 class Factory
 {
     public:
-        Factory();
-        virtual ~Factory();
+        // Default Constructor
+        Factory();     
+        // Destructor: 
+        ~Factory();
     protected:
+        // Protected hash function for use in derived factory classes
         virtual int hash(char) const;
 };
 
