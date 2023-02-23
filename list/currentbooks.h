41ac5c8e5cbc47ed612d6bda7f95c95f4b275106
wbach <91107968+Will-Bach90@users.noreply.github.com>
2023-02-23 03:13:02 -0800

    Update currentbooks.h

--- list/currentbooks.h
+++ list/currentbooks.h
@@ -1,3 +1,14 @@
+//---------------------------------------------------------------------------
+// currentbooks.h
+// Derived list class for saving all of patron's checked out books in linked
+// list
+//---------------------------------------------------------------------------
+// CurrentBooks class Implementation and assumptions:  
+//   -- Used as data member in Patron class
+//   -- Allows append, retrieve, and remove of given books from list
+//   -- Remove does not delete Node data member, only node itself.
+//
+//---------------------------------------------------------------------------
 #ifndef CURRENT_BOOKS
 #define CURRENT_BOOKS
 
@@ -20,11 +31,17 @@ class CurrentBooks : public List
         //---------------------------------------------------------------------
         // Virtual function to remove book from list
         virtual bool remove(Item*);
+        
+        //---------------------------------------------------------------------
+        // Retrieves a book item node contained in the linked list 
+        bool retrieve(const Item&, Item*&) const;
 
         //---------------------------------------------------------------------
         // Function to print out entire list of books currently borrowed
         virtual void print() const;
 
+        //---------------------------------------------------------------------
+        // Deletes entire linked list
         virtual void makeEmpty();
 
     private:
